247744c54fe6c23bbcbca07080c1ff3ffce5a944
simiadebowale <47654119+simiadebowale@users.noreply.github.com>
2019-02-15 01:55:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const request = require("request");
 const express = require("express");
 const app = express();
 
+//app setup
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
 app.get("/",function(req, res){
     res.render("search");
 });
 
+//results Route
 app.get("/results",(req, res)=>{
+    //creating a variable with + the api key needed to access data
     var query = req.query.search + "&apikey=thewdb";
     var url = "http://www.omdbapi.com/?s=" + query ;
     
+    //api request
     request(url, (error, response, body) =>{
     if(!error && response.statusCode === 200){
         var parsedData = JSON.parse(body);
